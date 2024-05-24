7444bf32284c3463d8eb8bf861576992975ae8b0
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-05-24 17:36:24 +0300

    color

--- lib/models/background_color.dart
+++ lib/models/background_color.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+// A model class representing a background color with a gradient effect
 class BackgroundColor {
+  // The starting color of the gradient
   final Color begin;
+  // The ending color of the gradient
   final Color end;
+
+  // Constructor for the BackgroundColor class
+  // The 'required' keyword ensures that both 'begin' and 'end' are mandatory parameters
   BackgroundColor({
     required this.begin,
     required this.end,
